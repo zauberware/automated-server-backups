b4a44093180b2685fcea9d48554289a0cb6c1d29
Simon Franzen <simon@zauberware.com>
2018-03-26 02:27:27 +0200

    Better email layout

--- backup.sh
+++ backup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 ###################################
 #
-# Automated server backups
+# Backup to NFS mount script.
 #
 ###################################
 
-# what to backup
-# backup folders
-backup_files="/var/www/my-website.de /var/www/wordpress /etc"
+# give your script a name
+host=hostname
 
-# backup databases
-backup_databases="mywebsite wordpress"
+# what to backup
+backup_files="/var/www/wordpress /var/www/mywebsite"
+#backup_files="/home/deploy/repositories /etc"
+
+# which databases
+backup_databases="wordpress mywebsite"
 
 # where to backup
 dest="/mnt/backup"
@@ -20,44 +23,68 @@ mkdir -p $dest
 
 # create archive filenames
 day=$(date +%y-%m-%d)
-hostname=$(hostname -s)
-archive_file="$hostname-$day.tar"
-mysql_file="$hostname-mysql-$day.tar"
+archive_file="$host-$day.tar"
+mysql_file="$host-mysql-$day.tar"
 
 # print start status message
-echo "Backing up $backup_files to $dest/$archive_file ..."
-echo "Backing up $backup_databases to $dest/$mysql_file ..."
-date
-echo
-echo "================================================================"
-echo
-echo "Download database backup with $ scp backup@185.102.93.107:backups/$mysql_file backup"
-echo
+echo "Dear $host product owner,"
+echo ""
+echo "a new backup of your server was generated on your server." 
+echo "You can find the files and folder under $dest/$archive_file and databases here $dest/$mysql_file. The backup included the follwing data:"
+echo ""
+echo "Folders: $backup_files"
+echo "Databases: $backup_databases"
+echo ""
+echo ""
+echo ""
+echo "==================== DOWNLOAD WITH SCP ======================"
+echo ""
+echo "SQL database"
+echo "Download database backup with $ scp root@111.222.333.444:/backups/$mysql_file backup"
 echo "Unpack files with $ tar -xvzf backup/$mysql_file"
-echo
-echo "Download the full backup with with $ scp backup@185.102.93.107:backups/$archive_file backup"
-echo
+echo ""
+echo "Files and Folders:"
+echo "Download the full backup with with $ scp root@111.222.333.444:/backups/$archive_file backup"
 echo "Unpack files with $ tar -xvzf backup/$archive_file"
 echo 
-echo "================================================================="
+echo "====================== FTP DOWNLOAD ========================="
 echo ""
+echo "Acces with FTP:"
+echo "USER: root"
+echo "PASS: X"
+echo "SERV: 111.222.333.444"
+echo "Download with FTP client manually"
+echo ""
+echo "=============================================================="
+echo ""
+echo ""
+echo "I wish you a nice day !"
+echo ""
+echo ""
+echo "========================== LOG ==============================="
+echo ""
+echo "Starting script..."
+date
 
-# database dump in temp file
+# backup the files using tar.
 tar czvfP $dest/$archive_file $backup_files
 
-# pack the sql dump with tar and remove dump
+# database backup 
 mysqldump --user root --routines --triggers --single-transaction --databases $backup_databases > "$dest/sql_dump.sql"
-tar czfP $dest/$mysql_file "$dest/sql_dump.sql"
-rm $dest/sql_dump.sql
+tar czfP $dest/$mysql_file "$dest/sql_dump.sql" && rm $dest/sql_dump.sql
 
 # print end status message
-echo
-echo "Backup SUCCESS"
+echo "."
+echo "."
+echo "."
+echo "... Backup SUCCESS!"
 date
-echo
-echo "Delete old files"
+echo ""
+echo "Delete old files !"
+find $dest -mtime +14 -type f
 find $dest -mtime +14 -type f -delete
-echo
+echo ""
 
-# echo generated files
+# long listing of files in $dest to check file sizes
+echo "Files generated"
 ls -lh $dest
